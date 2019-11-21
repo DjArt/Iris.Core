f8fcc859e934ad22e32e8c310264478d31ca860e
DjArt <art.letter.mail@gmail.com>
2019-11-21 21:19:39 +0700

    standardheader added

---
+++ Iris.Core/Network/Packets/StandardHeader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iris.Core.Network.Packets
+{
+    public struct StandardHeader
+    {
+        public bool IsConnection;
+        public IrisAccessPoint Sender;
+        public IrisAccessPoint Recipient;
+        public byte[] Message;
+    }
+}
