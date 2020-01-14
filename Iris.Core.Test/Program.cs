dbbedb43be8e2e68e6088c26d01c700dea87371c
DjArt <art.letter.mail@gmail.com>
2020-01-15 00:12:37 +0700

    tests platform changed to .netcoreapp

---
+++ Iris.Core.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Iris.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
